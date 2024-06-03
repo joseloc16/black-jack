f1d19249b39ef5417c0ed597ff0215b5bf957deb
jnunez <darksoul.js17@gmail.com>
2024-06-03 00:24:56 -0500

    se crean funciones para pedir carta y obtener el valor de la carta

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -9,6 +9,7 @@ let deck = [];
 const tipos = ['C', 'D', 'H', 'S'];
 const especiales = ['A', 'J', 'Q', 'K']
 
+// Esta función crea una nueva baraja
 const crearDeck = () => {
     
   for(let i = 2; i <= 10; i++) {
@@ -22,11 +23,38 @@ const crearDeck = () => {
       deck.push(esp + tipo);
     }
   }
-
-  console.log(deck);
   deck = _.shuffle(deck);
-  console.log(deck);
 }
 
-
 crearDeck();
+
+// Esta función nos permite tomar una carta
+const pedirCarta = () => {
+  if(deck.length === 0) {
+    throw 'No hay cartas en el deck';
+  }
+  let carta = deck.shift();
+  return carta;
+}
+
+pedirCarta();
+
+const valorCarta = (carta) => {
+  let valor = carta.substring(0, carta.length - 1);
+
+  if(isNaN(valor)) {
+    if(valor === especiales[0]) {
+      valor = 1;
+    } else if(valor === especiales[1]) {
+      valor = 11;
+    } else if(valor === especiales[2]) {
+      valor = 12;
+    } else {
+      valor = 13;
+    }
+  }
+  return valor * 1;
+}
+
+let valorCard = valorCarta('KD');
+console.log(valorCard);
